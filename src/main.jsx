c1610db04eaa4b201616b01e854191fd2ca78dae
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-20 19:14:24 -0600

    VODOO: Moved navbar and router to a central app

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppRouter from './AppRouter'
-
-import { Navbar } from './features/navigation/components/Navbar'
+import { App } from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    <AppRouter />
+    <App />
   </StrictMode>,
 );
