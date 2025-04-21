a588752d69f14a638ce4fd6a33e3b2ca58481e47
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-20 22:19:06 -0600

    VODOO: Navbar can be shown inside the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
+import { useNavbar } from '../features/navigation/hooks/useNavbar';
 
 export const Home = () => {
+    const { setShowNavbar } = useNavbar();
+
     let connected = () => {
+        setShowNavbar(true);
     };
 
     useEffect(() => {
+        console.log("connected home");
         connected();
     }, [])
-    
 
     return (
         <div className="v-module-container">
