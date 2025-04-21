8d55c88488d55e1d7c048352fbda34864d7eeccb
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-20 19:21:47 -0600

    VODOO: Added login page to router

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,7 @@
 // src/app/AppRouter.tsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
 
 "use strict";
 
@@ -8,7 +9,8 @@ export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
