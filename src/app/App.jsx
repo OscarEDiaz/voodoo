8dccd1a699e225559e14f7d24e208c947ccfe158
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-20 22:17:26 -0600

    VODOO: User needs to be logged in so navbar can be shown

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,14 +3,17 @@ import AppRouter from './AppRouter';
 
 import { Navbar } from '../features/navigation/components/Navbar';
 import { useNavbar } from '../features/navigation/hooks/useNavbar';
+import { BrowserRouter } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
 export const App = () => {
     const { showNavbar, pageTitle } = useNavbar();
+    const { user } = useAuth();
 
     return (
-        <>
-            { showNavbar && <Navbar pageName={pageTitle} /> }
+        <BrowserRouter>
+            { (showNavbar && user) && <Navbar pageName={pageTitle} /> }
             <AppRouter />
-        </>
+        </BrowserRouter>
     );
 };
