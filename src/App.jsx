e14ef5a1a37428ce23726c6e4da2f9955089b16c
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-20 19:16:25 -0600

    VODOO: Added conditional nav rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import { useUIStore } from './store/useUIStore';
 import { Navbar } from './features/navigation/components/Navbar';
 
 export const App = () => {
-    const showNavbar = useUIStore((state) => state.showNavbar)
+    const showNavbar = useUIStore((state) => state.showNavbar);
+    const pageTitle = useUIStore((state) => state.pageTitle);
 
     return (
         <>
-            <Navbar  />
+            { showNavbar && <Navbar pageName={pageTitle} /> }
             <AppRouter />
         </>
     );
