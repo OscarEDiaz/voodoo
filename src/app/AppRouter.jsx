7487a19cdc545890734f9621a987499106ba02cc
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-20 22:17:43 -0600

    VODOO: Changed render logic

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,18 +1,28 @@
 // src/app/AppRouter.tsx
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "../pages/home/Home";
-import { Login } from "../pages/Login/Login";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home, Login } from "../pages";
+import { useAuth } from "../hooks/useAuth";
+import { routeConstants } from "../shared/constants";
 
 "use strict";
 
 export default function AppRouter() {
+  const { user } = useAuth();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path={routeConstants.UI_ROUTE_LOGIN}
+        element={ user ? <Navigate to={routeConstants.UI_ROUTE_HOME} replace /> : <Login /> }
+      />
+      <Route
+        path={routeConstants.UI_ROUTE_HOME}
+        element={ user ? <Home /> : <Navigate to={routeConstants.UI_ROUTE_LOGIN} replace /> }
+      />
+      <Route
+        path="*"
+        element={ <Navigate to={routeConstants.UI_ROUTE_LOGIN} replace /> }
+      />
+    </Routes>
   );
 };
