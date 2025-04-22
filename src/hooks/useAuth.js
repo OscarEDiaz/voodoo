2c65570a5ba21b834c752e8e0d75b95d230375ee
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-21 20:11:24 -0600

    VODOO: improved auth hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,15 +2,21 @@ import { useState, useEffect } from "react";
 
 import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase/config";
+import { useAuthStore } from "../store/useAuthStore";
 
 "use strict";
 
 export function useAuth() {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const user       = useAuthStore((state) => state.user);
+    const setUser    = useAuthStore((state) => state.setUser);
+    const loading    = useAuthStore((state) => state.loading);
+    const setLoading = useAuthStore((state) => state.setLoading);
+    const error      = useAuthStore((state) => state.error);
+    const setError   = useAuthStore((state) => state.setError);
+    
+    const [errorTimeOut, setErrorTimeOut] = useState(null);
 
     useEffect(() => {
-        // Detecta si el usuario está logueado al cargar la app
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
             setLoading(false);
@@ -19,22 +25,55 @@ export function useAuth() {
         return () => unsubscribe();
     }, []);
 
-    const login = async (email, password, setError) => {
+    /**
+     * @function errorHandler
+     * @description Handle errors related to firebase auth API
+     * @param {object} error Firebase error
+     * @returns undefined
+     */
+    const errorHandler = (error) => {
+        if (errorTimeOut)
+            clearTimeout(errorTimeOut);
+
+        setError(error.message);
+
+        const timeOut = setTimeout(() => {
+            setError("");
+            setErrorTimeOut(null);
+        }, 4000);
+
+        setErrorTimeOut(timeOut);
+    }
+
+    /**
+     * @function login
+     * @description Use firebase auth API to log in.
+     * @param {string} email User email
+     * @param {string} password User password
+     * @returns undefined
+     */
+    const login = async (email, password) => {
         try {
+            setError("");
             await signInWithEmailAndPassword(auth, email, password);
         } catch (error) {
-            console.log("error", {error});
-            setError(error.message);
+            errorHandler(error);
         }
     };
-
+    
+    /**
+     * @function logout
+     * @description Use firebase auth API to log out.
+     * @returns undefined
+     */
     const logout = async () => {
         try {
+            setError("");
             await signOut(auth);
         } catch (error) {
-            console.error(error);
+            errorHandler(error);
         }
     };
 
-    return { user, loading, login, logout };
+    return { user, loading, error, login, logout };
 }
