e15c263357d493cdaf51422dfddcbba3036077ec
Oscar Diaz <oscarrmzdiaz@gmail.com>
2025-04-21 20:09:20 -0600

    VODOO: Finished login form

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAuth } from '../../../hooks/useAuth';
 import { useForm } from 'react-hook-form';
-import { loginConstants, routeConstants } from '../../../shared/constants';
+import { loginConstants } from '../../../shared/constants';
 import logo from '../../../assets/images/voodoo.png';
 import { useNavigate } from 'react-router-dom';
+import { paths } from '../../../app/routes';
+
 
 "use strict";
 
 export const LoginForm = () => {
     const navigate = useNavigate();
     const { register, handleSubmit } = useForm();
-    const [formError, setFormError] = useState("");
-    const { login } = useAuth();
+    const { error, login } = useAuth();
 
     const onSubmit = async (formData) => {
-        await login(formData.email, formData.password, setFormError);
-        navigate(routeConstants.UI_ROUTE_HOME);
+        await login(formData.email, formData.password);
+        navigate(paths.HOME_PATH);
     };
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='v-login-form'>
             <img src={logo} className='v-login-form-logo' />
             <input 
-                {...register("email")}
+                {...register("email", { required: true })}
                 type="email"
                 placeholder={ loginConstants.UI_STR_EMAIL }
             />
             <input
-                {...register("password")}
+                {...register("password", { required: true })}
                 type="password"
                 placeholder={ loginConstants.UI_STR_PASSWORD }
             />
             <button type="submit">
                 { loginConstants.UI_STR_LOGIN }
             </button>
-            <p style={{marginTop: "30px"}}>{formError}</p>
+            <p style={{marginTop: "30px"}}>{error}</p>
         </form>
     );
 };
